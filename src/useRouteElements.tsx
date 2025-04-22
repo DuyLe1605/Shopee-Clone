f5fc208ca72b3abaee1041087301a993c989055a
DuyLe1605 <leminhduy160505@gmail.com>
2025-04-22 09:37:02 +0700

    Protected Route và Rejected Route

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,35 +1,65 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import ProductList from './pages/ProductList'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
+import Profile from './pages/Profile'
+
+const isLoggedIn = false
+
+function ProtectedRoute() {
+  return isLoggedIn ? <Outlet /> : <Navigate to='/login' />
+}
+function RejectedRoute() {
+  return isLoggedIn ? <Navigate to='/' /> : <Outlet />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
-      path: '/',
-      element: (
-        <MainLayout>
-          <ProductList />
-        </MainLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/',
+          element: (
+            <MainLayout>
+              <ProductList />
+            </MainLayout>
+          )
+        },
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
-    },
-    {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
   //
